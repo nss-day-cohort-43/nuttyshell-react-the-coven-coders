1578437ddc6c12c5195e7b7c44a747f160226497
ste163 <samtedwards@pm.me>
2020-10-09 16:52:01 -0400

    added authentication

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
 import { NutShell } from './NutShell'
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NutShell />
+    <Router>
+      <NutShell />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
