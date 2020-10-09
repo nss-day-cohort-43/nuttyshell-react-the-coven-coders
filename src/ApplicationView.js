013ddf8008af737df2fb74d4cd084036d02e2a64
Tristanwyatt <tristan7306@gmail.com>
2020-10-09 14:46:34 -0500

    Grid container

---
+++ src/ApplicationView.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import './NutShell.css'
+
+export const GridLayout = () => {
+  
+    return (
+        <Grid divided='vertically'className="container">
+            <Grid.Row columns={3} className="row1">
+            <Grid.Column className="row1-column1">
+                Chase fathom topgallant chandler tender cable take a caulk Spanish Main American Main scurvy.
+                Jack Tar jury mast chandler Privateer grog blossom sloop topmast parley doubloon fire ship. 
+                List driver lugsail interloper hornswaggle crow's nest pressgang lanyard jib squiffy.
+                Clipper black jack bowsprit grog blossom pink bilge water bilge rat Jack Tar fire in the hole bilged on her anchor. 
+                Schooner heave to grog clipper barque Shiver me timbers gangplank Brethren of the Coast cog no prey, no pay. 
+                Lookout rope's end lanyard dance the hempen jig crow's nest parley Buccaneer Nelsons folly tack chase.
+            </Grid.Column >
+            <Grid.Column className="row1-column2">
+                Chase fathom topgallant chandler tender cable take a caulk Spanish Main American Main scurvy.
+                Jack Tar jury mast chandler Privateer grog blossom sloop topmast parley doubloon fire ship. 
+                List driver lugsail interloper hornswaggle crow's nest pressgang lanyard jib squiffy.
+                Clipper black jack bowsprit grog blossom pink bilge water bilge rat Jack Tar fire in the hole bilged on her anchor. 
+                Schooner heave to grog clipper barque Shiver me timbers gangplank Brethren of the Coast cog no prey, no pay. 
+                Lookout rope's end lanyard dance the hempen jig crow's nest parley Buccaneer Nelsons folly tack chase.
+            </Grid.Column>
+            <Grid.Column className="row1-column3">
+                Chase fathom topgallant chandler tender cable take a caulk Spanish Main American Main scurvy.
+                Jack Tar jury mast chandler Privateer grog blossom sloop topmast parley doubloon fire ship. 
+                List driver lugsail interloper hornswaggle crow's nest pressgang lanyard jib squiffy.
+                Clipper black jack bowsprit grog blossom pink bilge water bilge rat Jack Tar fire in the hole bilged on her anchor. 
+                Schooner heave to grog clipper barque Shiver me timbers gangplank Brethren of the Coast cog no prey, no pay. 
+                Lookout rope's end lanyard dance the hempen jig crow's nest parley Buccaneer Nelsons folly tack chase.
+            </Grid.Column>
+            </Grid.Row>
+    
+            <Grid.Row columns={2} className="row2">
+            <Grid.Column className="row2-column1">
+                Chase fathom topgallant chandler tender cable take a caulk Spanish Main American Main scurvy.
+                Jack Tar jury mast chandler Privateer grog blossom sloop topmast parley doubloon fire ship. 
+                List driver lugsail interloper hornswaggle crow's nest pressgang lanyard jib squiffy.
+                Clipper black jack bowsprit grog blossom pink bilge water bilge rat Jack Tar fire in the hole bilged on her anchor. 
+                Schooner heave to grog clipper barque Shiver me timbers gangplank Brethren of the Coast cog no prey, no pay. 
+                Lookout rope's end lanyard dance the hempen jig crow's nest parley Buccaneer Nelsons folly tack chase.
+            </Grid.Column>
+            <Grid.Column className="row2-column2">
+                Chase fathom topgallant chandler tender cable take a caulk Spanish Main American Main scurvy.
+                Jack Tar jury mast chandler Privateer grog blossom sloop topmast parley doubloon fire ship. 
+                List driver lugsail interloper hornswaggle crow's nest pressgang lanyard jib squiffy.
+                Clipper black jack bowsprit grog blossom pink bilge water bilge rat Jack Tar fire in the hole bilged on her anchor. 
+                Schooner heave to grog clipper barque Shiver me timbers gangplank Brethren of the Coast cog no prey, no pay. 
+                Lookout rope's end lanyard dance the hempen jig crow's nest parley Buccaneer Nelsons folly tack chase.
+            </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    )
+}
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { NutShell } from './NutShell'
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
