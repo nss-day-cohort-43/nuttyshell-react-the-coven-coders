acbd680762b907de013b352492311e7c4fd5acf0
Audrey Thomasson <audrey@macbook-pro.lan>
2020-10-12 16:14:35 -0500

    working on public posts

--- src/components/posts/PublicPosts.js
+++ src/components/posts/PublicPosts.js
@@ -3,17 +3,17 @@ import { PostContext } from './PostProvider'
 
 export const PublicPosts = () => {
     const { posts, getPosts } = useContext(PostContext)
-    const activeUser = localStorage.getItem("activeUser")
+    const activeUser = +localStorage.getItem("activeUser")
 
     useEffect(() => {
         getPosts()
     }, [])
 
     const checkDate = (post) => {
-        const originalTimeStamp = `<span class="single__originalTimeStamp">posted at ${new Date(post.originalTimeStamp).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} -</span>`
-        const editedTimeStamp = `<span class="single__editedTimeStamp">edited at ${new Date(post.editedTimeStamp).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} -</span>`
+        const originalTimeStamp = <span className="single__originalTimeStamp">posted at {new Date(post.originalTimeStamp).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} -</span>
+        const editedTimeStamp = <span className="single__editedTimeStamp">edited at {new Date(post.editedTimeStamp).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} -</span>
         
-        return (post.editedTimeStamp === 0 ? `${originalTimeStamp}` : `${originalTimeStamp} ${editedTimeStamp}`)
+        return (post.editedTimeStamp === 0 ? {originalTimeStamp} : {originalTimeStamp}, {editedTimeStamp})
     }
 
     return (
@@ -24,24 +24,21 @@ export const PublicPosts = () => {
                     posts.map(post => {
                         // To reduce repeated strings, store username in variable
                         const username = <span key={post.user.id} className="single__username">{post.user.username}</span>
-
+console.log(post.post)
                         if (activeUser === post.userId) {
-                            return (
-                            <p key={post.id} className="post__single single__active">
+                            return <p key={post.id} className="post__single single__active">
                                 {username}
                                 {checkDate(post)}
                                 <span className="single__post">{post.post}</span> <button type="button">✎</button>
                                 <button type="button">✘</button>
                             </p>
-                            )
+                            
                         } else {
-                            return (
-                            <p key={post.id} className="post__single">
+                            return <p key={post.id} className="post__single">
                                 {username}
                                 {checkDate(post)}
                                 <span className="single__post">{post.post}</span>
                             </p>
-                            )
                         }
                     }).join("")
                 }
