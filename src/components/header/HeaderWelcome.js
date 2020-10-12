a79404ff56d931c5672fbe4be94054b5e33fe378
Tristanwyatt <tristan7306@gmail.com>
2020-10-12 12:58:28 -0500

    image logo/ images

--- src/components/header/HeaderWelcome.js
+++ src/components/header/HeaderWelcome.js
@@ -1,10 +1,14 @@
 import React from "react"
+import image from '../../images/The_Coven_Code_Ball_200x200.png'
 
 export const HeaderWelcome = () => {
 
     const username = localStorage.getItem("username")
-    
+
     return (
-        <p>Witchy Salutations, {username}!</p>    
+    <>
+        <img src={image} alt="logo"/>
+        <p>Witchy Salutations, {username}!</p>
+    </>  
     )
 }
