3e415b14afe808fa3ed190f45129bc961a3e873f
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-10-12 13:22:52 -0500

    added logout functionality

--- src/components/header/HeaderWelcome.js
+++ src/components/header/HeaderWelcome.js
@@ -1,14 +1,24 @@
 import React from "react"
+import { useHistory } from "react-router-dom"
 import image from '../../images/The_Coven_Code_Ball_200x200.png'
 
 export const HeaderWelcome = () => {
 
     const username = localStorage.getItem("username")
+    const history = useHistory()
+
+    const clearStorage = () => {
+        localStorage.clear()
+        history.push("/")
+    }
 
     return (
     <>
         <img src={image} alt="logo"/>
         <p>Witchy Salutations, {username}!</p>
+        <button onClick={clearStorage}>
+            Log Out
+        </button>
     </>  
     )
 }
