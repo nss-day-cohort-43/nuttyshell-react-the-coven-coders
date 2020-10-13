b16b88006a0616aca13e07c8f0c3c5e42a396a22
ste163 <samtedwards@pm.me>
2020-10-13 13:37:52 -0400

    added SVG logo

--- src/components/header/HeaderWelcome.js
+++ src/components/header/HeaderWelcome.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useHistory } from "react-router-dom"
-import image from '../../images/The_Coven_Code_Ball_200x200.png'
+import image from '../../images/logo_cc.svg'
 
 export const HeaderWelcome = () => {
 
@@ -14,7 +14,7 @@ export const HeaderWelcome = () => {
 
     return (
     <>
-        <img src={image} alt="logo"/>
+        <img src={image} alt="Coven Coders logo"/>
         <p>Witchy Salutations, {username}!</p>
         <button onClick={clearStorage}>
             Log Out
