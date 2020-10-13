f8a8cad15bc967f5e61c4e1d1906093bf4836a2f
Audrey Thomasson <audrey@macbook-pro.lan>
2020-10-13 09:58:55 -0500

    updated tab names

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -6,13 +6,13 @@ import { PublicPosts } from './PublicPosts'
 
 const panes = [
   {
-    menuItem: { key: 'users', icon: 'users', content: 'Users' },
+    menuItem: { key: 'users', icon: 'users', content: 'Public' },
     render: () => <Tab.Pane><PublicPosts/></Tab.Pane>,
   },
   {
     menuItem: (
       <Menu.Item key='messages'>
-        Messages<Label>15</Label>
+        Secret Messages
       </Menu.Item>
     ),
     render: () => <Tab.Pane>Chase fathom topgallant chandler tender cable take a caulk Spanish Main American Main scurvy.
