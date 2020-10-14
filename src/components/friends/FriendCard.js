aa6c76026d47be9c489093d0abc18f7ffc0d240d
ste163 <samtedwards@pm.me>
2020-10-13 20:07:03 -0400

    fixed class instead of className

--- src/components/friends/FriendCard.js
+++ src/components/friends/FriendCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const FriendCard = props => (
     <div className="friend__card">
-        <span class="friend__name">{props.props}</span>
+        <span className="friend__name">{props.props}</span>
         <button className="friend__delete">✘</button>
     </div>
 )
